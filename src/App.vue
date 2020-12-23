ecfe4a4051d25027959ba079f73ddfa4508f7bfd
NorinMp143 <madanparmar637@gmail.com>
2020-12-23 11:33:13 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <form @submit.prevent="onSubmit">
+  <div class="form-group">
+    <label for="string">Enter Any String</label><br>
+    <input type="text" id="string" class="form-control" v-model="data.string"><br>
+    <button type="submit">Click Me</button>
+  </div>
+  </form>
+  <br>
+  <StringForm :msg="data.str"/>
+</template>
+
+<script>
+import StringForm from './components/StringForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    StringForm
+  },
+  data() {
+    return {
+        data: {
+            string: '',
+            str: ''
+        }
+    }
+  },
+  methods:{
+    onSubmit(){
+      this.data.str=this.data.string.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/StringForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="string">
+    {{msg}}    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StringForm',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
